99d2460996963b0260a83c0d8990404da0d6a75f
Mykhailo Datsko <mike@datsko.net>
2019-01-26 01:05:03 +0200

    fixed mobile and tablet versions

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, Input } from '@angular/core';
 import { HomeComponent } from './home.component';
+
+@Component({selector: 'app-carousel', template: ``}) class CarouselComponent { @Input() options: any[]; @Input() slides: any[]; }
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   @Input() menu: any[];
+  isShowMobileMenu: boolean = false;
 
   constructor() { }
 
